be437cb72af10724dae902a43b69810c3c0f646c
Inve <inveniahadouken@gmail.com>
2019-08-07 22:03:53 -0300

    react

---
+++ prueba-react/src/index.js
@@ -0,0 +1,23 @@
+/**
+ * React6 se separa en : react y react-dom
+ * 
+ * 
+ * 1 - Tiene que siempre estar la variable React en scope
+ * 2 - Un componente de React es una función
+ * 3 - Todos los componentes de react tienen que retornar algo : React.component, JSX
+ */
+
+ //ES6 import React from "react"
+ //CommonJS 
+ //-> const React = require("react") 
+
+ import React from "react" 
+ import ReactDOM from "react-dom"
+
+ let App = () => "Ble"
+
+ ReactDOM.render(
+     <App>
+     </App>,
+    document.getElementById('root')
+ )
